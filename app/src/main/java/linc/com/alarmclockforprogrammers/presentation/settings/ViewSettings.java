6dd43e5385df47ded28d82bf5b4e816c863fd441
Andrew <andriy.serb1@gmail.com>
2019-08-30 18:56:58 +0300

    Settings screen model

--- app/src/main/java/linc/com/alarmclockforprogrammers/presentation/settings/ViewSettings.java
+++ app/src/main/java/linc/com/alarmclockforprogrammers/presentation/settings/ViewSettings.java
@@ -4,6 +4,8 @@ package linc.com.alarmclockforprogrammers.presentation.settings;
 public interface ViewSettings {
 
     void setSelectedTheme(boolean isDarkTheme);
+    void openRateActivity();
+    void openMessageActivity();
     void disableDrawerMenu();
     void restartActivity();
     void openAlarmsFragment();
--- app/src/main/java/linc/com/alarmclockforprogrammers/ui/fragments/settings/FragmentSettings.java
+++ app/src/main/java/linc/com/alarmclockforprogrammers/ui/fragments/settings/FragmentSettings.java
@@ -1,5 +1,6 @@
 package linc.com.alarmclockforprogrammers.ui.fragments.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import linc.com.alarmclockforprogrammers.R;
 import linc.com.alarmclockforprogrammers.model.data.preferences.PreferencesAlarm;
@@ -47,17 +50,37 @@ public class FragmentSettings extends BaseFragment implements ViewSettings,
         toolbar.setNavigationOnClickListener(this);
         this.presenter.setData();
 
+
+        TextView tv = view.findViewById(R.id.settings__rate_us);
+        tv.setOnClickListener(this);
+
         return view;
     }
 
     @Override
     public void onClick(View v) {
-        this.presenter.saveTheme(switchTheme.isChecked());
+//        this.presenter.saveTheme(switchTheme.isChecked());
     }
 
     @Override
     public void setSelectedTheme(boolean isDarkTheme) {
         this.switchTheme.setChecked(isDarkTheme);
+    }
+
+    @Override
+    public void openRateActivity() {
+
+    }
+
+    @Override
+    public void openMessageActivity() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("plain/text");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[] { "linc.apps.sup@gmail.com" });
+        intent.putExtra(Intent.EXTRA_SUBJECT, "subject");
+        intent.putExtra(Intent.EXTRA_TEXT, "mail body");
+        startActivity(Intent.createChooser(intent, ""));
+
     }
 
     @Override
