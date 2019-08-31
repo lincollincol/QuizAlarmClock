c025904538232735949886ff3d6a708d1b5be299
Andrew <andriy.serb1@gmail.com>
2019-08-31 20:55:10 +0300

    Settings fragment works: rate and support

--- app/src/main/java/linc/com/alarmclockforprogrammers/ui/fragments/settings/FragmentSettings.java
+++ app/src/main/java/linc/com/alarmclockforprogrammers/ui/fragments/settings/FragmentSettings.java
@@ -1,6 +1,8 @@
 package linc.com.alarmclockforprogrammers.ui.fragments.settings;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,7 +12,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import linc.com.alarmclockforprogrammers.R;
 import linc.com.alarmclockforprogrammers.model.data.preferences.PreferencesAlarm;
@@ -45,21 +46,38 @@ public class FragmentSettings extends BaseFragment implements ViewSettings,
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
         Toolbar toolbar = view.findViewById(R.id.settings__toolbar);
-        this.switchTheme = view.findViewById(R.id.settings__theme_switch);
+        LinearLayout darkModeLayout = view.findViewById(R.id.settings__dark_mode);
+        LinearLayout rateLayout = view.findViewById(R.id.settings__rate);
+        LinearLayout supportLayout = view.findViewById(R.id.settings__support);
+        this.switchTheme = view.findViewById(R.id.settings__dark_mode_toggle);
 
         toolbar.setNavigationOnClickListener(this);
+        darkModeLayout.setOnClickListener(this);
+        rateLayout.setOnClickListener(this);
+        supportLayout.setOnClickListener(this);
+        this.switchTheme.setOnClickListener(this);
+
         this.presenter.setData();
-
-
-        TextView tv = view.findViewById(R.id.settings__rate_us);
-        tv.setOnClickListener(this);
-
         return view;
     }
 
     @Override
     public void onClick(View v) {
-//        this.presenter.saveTheme(switchTheme.isChecked());
+        switch(v.getId()) {
+            case R.id.settings__dark_mode:
+                this.presenter.modeStateChanged();
+                break;
+            case R.id.settings__dark_mode_toggle:
+                this.presenter.modeStateChanged();
+                break;
+            case R.id.settings__rate:
+                this.presenter.rateApp();
+                break;
+            case R.id.settings__support:
+                this.presenter.sendMessage();
+                break;
+            default: this.presenter.saveTheme();
+        }
     }
 
     @Override
@@ -69,18 +87,25 @@ public class FragmentSettings extends BaseFragment implements ViewSettings,
 
     @Override
     public void openRateActivity() {
-
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("market://details?id=" + "com.android.chrome")));
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=" + "com.android.chrome")));
+        }
     }
 
     @Override
     public void openMessageActivity() {
+        // todo set message and subject
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("plain/text");
+        intent.setPackage("com.google.android.gm");
         intent.putExtra(Intent.EXTRA_EMAIL, new String[] { "linc.apps.sup@gmail.com" });
-        intent.putExtra(Intent.EXTRA_SUBJECT, "subject");
-        intent.putExtra(Intent.EXTRA_TEXT, "mail body");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "SUPPORT");
+        intent.putExtra(Intent.EXTRA_TEXT, "support !!!!!!!");
         startActivity(Intent.createChooser(intent, ""));
-
     }
 
     @Override
@@ -100,6 +125,7 @@ public class FragmentSettings extends BaseFragment implements ViewSettings,
 
     @Override
     public void onBackPressed() {
-        this.presenter.saveTheme(switchTheme.isChecked());
+        this.presenter.saveTheme();
     }
+
 }
